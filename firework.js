0f6cadfae91d43a55fb8c59af0833c0bff9924ba
Md Taseen Atehar <63313585+Seen3@users.noreply.github.com>
2022-10-11 15:59:15 +0530

    Update firework.js

--- firework.js
+++ firework.js
@@ -1,14 +1,13 @@
 function Firework(x, y) {
-    this.hu=random(255)
-    this.firework = new Particle(random(width), height,true,this.hu);
+    this.hu = random(255)
+    this.firework = new Particle(random(width), height, true, this.hu);
     this.exploded = false;
-    this.radii=random(10,46);
-    this.particles=[];
-    
+    this.radii = random(10, 46);
+    this.particles = [];
 
-    this.done=()=>{
-        if (this.exploded && this.particles.length===0)
-        {
+
+    this.done = () => {
+        if (this.exploded && this.particles.length === 0) {
             return true;
         }
         return false;
@@ -19,27 +18,25 @@ function Firework(x, y) {
         if (!this.exploded) {
             this.firework.applyForce(gravity);
             this.firework.update();
-            let s=random(1)>0.5?-1:1;
-	    this.firework.vel.x=s*random(2);
+            let s = random(1) > 0.5 ? -1 : 1;
+            this.firework.vel.x = s * random(2);
             if (this.firework.vel.y >= 0) {
                 this.exploded = true;
                 this.explode();
             }
         }
-        for(let i=this.particles.length-1;i>=0;i--)
-        {
+        for (let i = this.particles.length - 1; i >= 0; i--) {
             this.particles[i].applyForce(gravity);
             this.particles[i].update();
-            if(this.particles[i].done()){
-                this.particles.splice(i,1);
+            if (this.particles[i].done()) {
+                this.particles.splice(i, 1);
             }
         }
     }
-    this.explode=()=>{
-        let v=random(50,120);
-        for(let i=0;i<v;i++)
-        {
-            let p=new Particle(this.firework.pos.x,this.firework.pos.y,false,this.hu,this.radii);
+    this.explode = () => {
+        let v = random(50, 120);
+        for (let i = 0; i < v; i++) {
+            let p = new Particle(this.firework.pos.x, this.firework.pos.y, false, this.hu, this.radii);
             this.particles.push(p);
         }
     }
@@ -47,8 +44,7 @@ function Firework(x, y) {
         if (!this.exploded) {
             this.firework.show();
         }
-        for(let i=0;i<this.particles.length;i++)
-        {
+        for (let i = 0; i < this.particles.length; i++) {
             this.particles[i].show();
         }
 
